795619d1b13f251e73624b565c32a546f0bfbe26
Muhammad Rahadian Nugraha <mnungki@gmail.com>
2021-03-31 13:37:11 +0700

    update

--- TUGAS_3/belajar_laravel/routes/web.php
+++ TUGAS_3/belajar_laravel/routes/web.php
@@ -14,14 +14,20 @@ use App\Http\Controllers\ManagementUserController;
 |
 */
 
-Route::get('/', [ManagementUserController::class, 'index']);
+Route::get('/user', [ManagementUserController::class, 'index']);
 
 //Route::resource('user', [ManagementUserController::class, 'user']);  
 
-Route::get('/',[ManagementUserController::class, 'index']);
-Route::get('/create',[ManagementUserController::class, 'create']);
-Route::get('/store',[ManagementUserController::class, 'store']);
-Route::get('/show',[ManagementUserController::class, 'show']);
-Route::get('/edit',[ManagementUserController::class, 'edit']);
-Route::get('/update',[ManagementUserController::class, 'update']);
-Route::get('/destroy',[ManagementUserController::class, 'destroy']);
+// Route::get('/',[ManagementUserController::class, 'index']);
+// Route::get('/create',[ManagementUserController::class, 'create']);
+// Route::get('/store',[ManagementUserController::class, 'store']);
+// Route::get('/show',[ManagementUserController::class, 'show']);
+// Route::get('/edit',[ManagementUserController::class, 'edit']);
+// Route::get('/update',[ManagementUserController::class, 'update']);
+// Route::get('/destroy',[ManagementUserController::class, 'destroy']);
+
+
+
+// Route::get('/home', function(){
+//     return view("home");
+// });
