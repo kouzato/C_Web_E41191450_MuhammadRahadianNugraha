7494211b13f5ed313980a5c6d954fe6d2c39a7cc
nug <mnungki@gmail.com>
2021-04-21 13:50:21 +0700

    Update

---
+++ Tugas_6/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth::user()->role !=1){
+            return redirect()->route('error');
+        }
+        return $next($request);
+    }
+}
